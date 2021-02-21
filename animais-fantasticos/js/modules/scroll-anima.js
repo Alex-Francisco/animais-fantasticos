cea2ef1a00e28c3ca185fce3309bf52d019d46e7
Alex Francisco <alex2007f@gmail.com>
2021-02-21 17:34:53 -0300

    ...
    ...

--- animais-fantasticos/js/modules/scroll-anima.js
+++ animais-fantasticos/js/modules/scroll-anima.js
@@ -2,25 +2,30 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
+
     this.checkDistance = this.checkDistance.bind(this);
   }
 
+  // Pega a distância de cada item em relação
+  // ao topo do site
   getDistance() {
     this.distance = [...this.sections].map((section) => {
       const offset = section.offsetTop;
       return {
         element: section,
-        offset: Math.floor(offset - this.windowMetade), 
+        offset: Math.floor(offset - this.windowMetade),
       };
     });
   }
 
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
-        item.classList.add('ativo');
+        item.element.classList.add('ativo');
       } else if (item.element.classList.contains('ativo')) {
-        item.classList.remove('ativo');
+        item.element.classList.remove('ativo');
       }
     });
   }
@@ -34,6 +39,7 @@ export default class ScrollAnima {
     return this;
   }
 
+  // Remove o event de scroll
   stop() {
     window.removeEventListener('scroll', this.checkDistance);
   }
