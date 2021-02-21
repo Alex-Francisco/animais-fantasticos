9af4b219d47694291ac5242e0f25ad7f389362e6
Alex Francisco <alex2007f@gmail.com>
2021-02-21 10:32:22 -0300

    mudança

--- animais-fantasticos/js/script.js
+++ animais-fantasticos/js/script.js
@@ -30,5 +30,5 @@ initMenuMobile();
 initFuncionamento();
 initAnimacaoScroll();
 
-initFetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
